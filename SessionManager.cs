b6283bf3d8c6e7e5d2a79b12e020cdb5e42d99aa
Marcelo Valls <cmarcelovalls@gmail.com>
2026-04-21 20:06:52 -0300

    Primer commit

---
+++ SessionManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3_Actividad_Singleton
+{
+    internal class SessionManager
+    {
+        private static SessionManager instance;
+
+        private SessionManager() { }
+
+        public static SessionManager Instance()
+        {
+            if (instance == null)
+            {
+                instance = new SessionManager();
+            }
+            return instance;
+        }
+
+    }
+}
